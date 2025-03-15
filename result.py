d8421964cb9f5b200ed65eb55d361643bced025f
LetterGaunt <193929321+L3Gaunt@users.noreply.github.com>
2025-03-15 13:11:42 -0500

    result.py tracks errors

--- result.py
+++ result.py
@@ -1,7 +1,7 @@
 import os, json
 
 files = os.listdir('results')
-output = ["Model\tOverall\tEasy\tHard\tShort\tMedium\tLong"]
+output = ["Model\tOverall\tEasy\tHard\tShort\tMedium\tLong\tErrors"]
 compensated = False
 
 for file in files:
@@ -12,7 +12,13 @@ for file in files:
         pred_data = [json.loads(line) for line in open(filename, encoding='utf-8')]
     easy, hard, short, medium, long = 0, 0, 0, 0, 0
     easy_acc, hard_acc, short_acc, medium_acc, long_acc = 0, 0, 0, 0, 0
+    error_count = 0  # Initialize error count
+    
     for pred in pred_data:
+        if 'error' in pred:
+            error_count += 1  # Count errors
+            continue
+
         acc = int(pred['judge'])
         if compensated and pred["pred"] == None:
             acc = 0.25
@@ -34,6 +40,15 @@ for file in files:
             long_acc += acc
 
     name = '.'.join(file.split('.')[:-1])
-    output.append(name+'\t'+str(round(100*(easy_acc+hard_acc)/len(pred_data), 1))+'\t'+str(round(100*easy_acc/easy, 1))+'\t'+str(round(100*hard_acc/hard, 1))+'\t'+str(round(100*short_acc/short, 1))+'\t'+str(round(100*medium_acc/medium, 1))+'\t'+str(round(100*long_acc/long, 1)))
+    num_successful_queries = easy + hard
+    total_queries = num_successful_queries + error_count
+    output.append(name+'\t'+
+                  str(round(100*(easy_acc+hard_acc)/num_successful_queries, 1) if num_successful_queries != 0 else 'nan')+'\t'+
+                  str(round(100*easy_acc/easy, 1) if easy != 0 else 'nan')+'\t'+
+                  str(round(100*hard_acc/hard, 1) if hard != 0 else 'nan')+'\t'+
+                  str(round(100*short_acc/short, 1) if short != 0 else 'nan')+'\t'+
+                  str(round(100*medium_acc/medium, 1) if medium != 0 else 'nan')+'\t'+
+                  str(round(100*long_acc/long, 1) if long != 0 else 'nan')+'\t'+
+                  str(error_count))
 
 open('result.txt', 'w', encoding='utf-8').write('\n'.join(output))
